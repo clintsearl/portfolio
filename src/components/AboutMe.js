4c160084a855adc16c0921be0fd88a6edc1431e1
clintsearl <clintsearl@gmail.com>
2019-07-09 16:00:27 -0600

    Added in react-router and routes for a few pages

---
+++ src/components/AboutMe.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AboutMe =()=>{
+    return(
+    <h1>Little Bit About Me</h1>
+    )
+}
+
+export default AboutMe;
---
+++ src/components/NoPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NoPage =()=>{
+    return(
+    <h1>Sorry I haven't made this page yet. </h1>
+    )
+}
+
+export default NoPage;
